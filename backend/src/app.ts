d458e06e4d2e84c50c52ef3601981f5da6e20ff9
Rupe88 <chyrupesh828@gmail.com>
2024-05-31 23:09:40 +0545

    typeorm db connected successfully

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
+import { AppDataSource } from "./database/data-source";
 dotenv.config();
 const app = express();
 
@@ -13,8 +14,16 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(process.env.APP_PORT || 3000, () => {
-  console.log(
-    `server is running on http://localhost:${process.env.APP_PORT || 3000}`
-  );
-});
+//db connection
+AppDataSource.initialize()
+  .then(() => {
+    console.log("database is connected successfully");
+    app.listen(process.env.APP_PORT || 3000, () => {
+      console.log(
+        `server is running on http://localhost:${process.env.APP_PORT || 3000}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log("error min db connection", error);
+  });
